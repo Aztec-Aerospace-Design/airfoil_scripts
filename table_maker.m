fd38fcb929428fdb9e153c0765449cee694cd058
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-05-29 15:58:47 -0700

    table_maker: throw exception if inputs aren't equal length

--- table_maker.m
+++ table_maker.m
@@ -1,6 +1,13 @@
 %% create yourself a table of all values from xfoil without breaking a sweat.
 
 function [output_array] = table_maker(airfoil_names, xfoil_output)
+    if (length(airfoil_names) ~= length(xfoil_output))
+        ME = MException('table_maker:arrayLengthMismatch', ...
+            'Expected the length of both inputs to be equal!');
+
+        throw(ME);
+    end
+
     output_array = {};
 
     for i = 1:length(airfoil_names)
